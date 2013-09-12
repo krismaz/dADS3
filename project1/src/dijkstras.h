b166def9e18c6db663830f56a47168b3e6b980ef
Jan H. Knudsen <jan.h.knudsen@gmail.com>
2013-09-12 09:49:17 -0700

    removed backup files and fixed indentation mess

--- project1/src/dijkstras.h
+++ project1/src/dijkstras.h
@@ -8,99 +8,99 @@
 
 struct DijkstraNode
 {
-    int Id;
-    int Dist;
-    DijkstraNode * Pred;
-    forward_list<pair<int, DijkstraNode*>>* Neighbours;
-    void* NodeData;
-    bool Visited;
+	int Id;
+	int Dist;
+	DijkstraNode * Pred;
+	forward_list<pair<int, DijkstraNode*>>* Neighbours;
+	void* NodeData;
+	bool Visited;
 
-    DijkstraNode()
-    {
-        Neighbours = new forward_list<pair<int, DijkstraNode*>>();
-        Visited = false;
-        Pred = NULL;
-    }
-    ~DijkstraNode()
-    {
-        delete Neighbours;   
-    }
+	DijkstraNode()
+	{
+		Neighbours = new forward_list<pair<int, DijkstraNode*>>();
+		Visited = false;
+		Pred = NULL;
+	}
+	~DijkstraNode()
+	{
+		delete Neighbours;   
+	}
 };
 
 void BinDijkstra(DijkstraNode ** nodes, int count)
 {
-    Heap<DijkstraNode*>* heap = new Heap<DijkstraNode*>(count);
-    Node<DijkstraNode*>* n = heap->Insert(0, nodes[0]);
-    nodes[0]->NodeData = n; 
-    nodes[0]->Dist = 0;
-    for(int i = 1; i < count;i++)
-    {
-        n = heap->Insert(INT_MAX, nodes[i]);
-        nodes[i]->NodeData = n;
-        nodes[i]->Dist = INT_MAX;
-    }
-    while(!heap->IsEmpty())
-    {      
-        Node<DijkstraNode*>* n = heap->DeleteMin();
-        DijkstraNode* dn = n->Val;
-        dn->Dist = n->Key;
-        dn->Visited = true;
-        if(n->Key == INT_MAX)
-        {
-            cout << "WARNING UNREACHABLE NODE!"<<endl;
-            return;
-        }
-        for(auto it = dn->Neighbours->begin(); it != dn->Neighbours->end(); ++it)
-        {
-            if((*it).second->Visited)
-            {
-                continue;
-            }
-            if(heap->DecreaseKey((Node<DijkstraNode*>*)(*it).second->NodeData, dn->Dist + (*it).first))
-            {
-                (*it).second->Pred = dn;
-            }
-        }
-        delete n;
-    }
+	Heap<DijkstraNode*>* heap = new Heap<DijkstraNode*>(count);
+	Node<DijkstraNode*>* n = heap->Insert(0, nodes[0]);
+	nodes[0]->NodeData = n; 
+	nodes[0]->Dist = 0;
+	for(int i = 1; i < count;i++)
+	{
+		n = heap->Insert(INT_MAX, nodes[i]);
+		nodes[i]->NodeData = n;
+		nodes[i]->Dist = INT_MAX;
+	}
+	while(!heap->IsEmpty())
+	{
+		Node<DijkstraNode*>* n = heap->DeleteMin();
+		DijkstraNode* dn = n->Val;
+		dn->Dist = n->Key;
+		dn->Visited = true;
+		if(n->Key == INT_MAX)
+		{
+			cout << "WARNING UNREACHABLE NODE!"<<endl;
+			return;
+		}
+		for(auto it = dn->Neighbours->begin(); it != dn->Neighbours->end(); ++it)
+		{
+			if((*it).second->Visited)
+			{
+				continue;
+			}
+			if(heap->DecreaseKey((Node<DijkstraNode*>*)(*it).second->NodeData, dn->Dist + (*it).first))
+			{
+				(*it).second->Pred = dn;
+			}
+		}
+		delete n;
+	}
 }
 
 void FibDijkstra(DijkstraNode ** nodes, int count)
 {
-    FibHeap<DijkstraNode*>* heap = new FibHeap<DijkstraNode*>();
-    FibNode<DijkstraNode*>* n = heap->Insert(0, nodes[0]);
-    nodes[0]->NodeData = n; 
-    nodes[0]->Dist = 0;
-    for(int i = 1; i < count;i++)
-    {
-        n = heap->Insert(INT_MAX, nodes[i]);
-        nodes[i]->NodeData = n;
-        nodes[i]->Dist = INT_MAX;
-    }
-    while(!heap->IsEmpty())
-    {      
-        FibNode<DijkstraNode*>* n = heap->DeleteMin();
-        DijkstraNode* dn = n->Val;
-        dn->Dist = n->Key;
-        dn->Visited = true;
-        if(n->Key == INT_MAX)
-        {
-            cout << "WARNING UNREACHABLE NODE!"<<endl;
-            return;
-        }
-        for(auto it = dn->Neighbours->begin(); it != dn->Neighbours->end(); ++it)
-        {
-            if((*it).second->Visited)
-            {
-                continue;
-            }
-            if(heap->DecreaseKey((FibNode<DijkstraNode*>*)(*it).second->NodeData, dn->Dist + (*it).first))
-            {
-                (*it).second->Pred = dn;
-            }
-        }
-        delete n;
-    }
+	FibHeap<DijkstraNode*>* heap = new FibHeap<DijkstraNode*>();
+	FibNode<DijkstraNode*>* n = heap->Insert(0, nodes[0]);
+	nodes[0]->NodeData = n; 
+	nodes[0]->Dist = 0;
+	for(int i = 1; i < count;i++)
+	{
+		n = heap->Insert(INT_MAX, nodes[i]);
+		nodes[i]->NodeData = n;
+		nodes[i]->Dist = INT_MAX;
+	}
+	while(!heap->IsEmpty())
+	{
+		FibNode<DijkstraNode*>* n = heap->DeleteMin();
+		DijkstraNode* dn = n->Val;
+		dn->Dist = n->Key;
+		dn->Visited = true;
+		if(n->Key == INT_MAX)
+		{
+			cout << "WARNING UNREACHABLE NODE!"<<endl;
+			return;
+		}
+		for(auto it = dn->Neighbours->begin(); it != dn->Neighbours->end(); ++it)
+		{
+			if((*it).second->Visited)
+			{
+				continue;
+			}
+			if(heap->DecreaseKey((FibNode<DijkstraNode*>*)(*it).second->NodeData, dn->Dist + (*it).first))
+			{
+				(*it).second->Pred = dn;
+			}
+		}
+		delete n;
+	}
 }
 
 #endif
--- project1/src/project1.cpp
+++ project1/src/project1.cpp
@@ -9,31 +9,31 @@
 using namespace std;
 
 int main(){
-    
-    cout << "Hello world!" << endl;
-    FibHeap<int> * h = new FibHeap<int>(10);
-    h->Insert(0,0);
-    cout << h->DeleteMin()->Val << endl;
-    h->Insert(8,3);
-    auto n = h->Insert(9,4);
-    h->DecreaseKey(n,18);
-    h->Insert(2,2);
-    h->Insert(1,1);
-    cout << h->DeleteMin()->Val << endl;
-    cout << h->DeleteMin()->Val << endl;
-    cout << h->DeleteMin()->Val << endl;
-    cout << h->DeleteMin()->Val << endl;
-    auto n2 = h->Insert(9,5);
-    auto n3 = h->Insert(32,8);
-    h->DecreaseKey(n2,0);
-    h->Insert(1,6);
-    h->Insert(5,7);
-    h->DecreaseKey(n3,17);
-    h->DecreaseKey(n3,7);
-    cout << h->DeleteMin()->Val << endl;
-    cout << h->DeleteMin()->Val << endl;
-    cout << h->DeleteMin()->Val << endl;
-    cout << h->DeleteMin()->Val << endl;
-    cin.get();
+	
+	cout << "Hello world!" << endl;
+	FibHeap<int> * h = new FibHeap<int>(10);
+	h->Insert(0,0);
+	cout << h->DeleteMin()->Val << endl;
+	h->Insert(8,3);
+	auto n = h->Insert(9,4);
+	h->DecreaseKey(n,18);
+	h->Insert(2,2);
+	h->Insert(1,1);
+	cout << h->DeleteMin()->Val << endl;
+	cout << h->DeleteMin()->Val << endl;
+	cout << h->DeleteMin()->Val << endl;
+	cout << h->DeleteMin()->Val << endl;
+	auto n2 = h->Insert(9,5);
+	auto n3 = h->Insert(32,8);
+	h->DecreaseKey(n2,0);
+	h->Insert(1,6);
+	h->Insert(5,7);
+	h->DecreaseKey(n3,17);
+	h->DecreaseKey(n3,7);
+	cout << h->DeleteMin()->Val << endl;
+	cout << h->DeleteMin()->Val << endl;
+	cout << h->DeleteMin()->Val << endl;
+	cout << h->DeleteMin()->Val << endl;
+	cin.get();
 	return 0;
 }
