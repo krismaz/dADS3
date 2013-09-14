a84f4729ecfed693b32a3cee96ad15a1262ced2e
Kris Ebbesen <krislinux@gmail.com>
2013-09-14 17:11:11 +0200

    Major speed improvements to the Fibonacci Heap

--- project1/src/fibheap.h
+++ project1/src/fibheap.h
@@ -4,6 +4,7 @@
 #include <forward_list>
 #include <unordered_set>
 #include <algorithm>
+#include <list>
 
 using namespace std;
 
@@ -14,11 +15,14 @@ struct FibNode
     T Val;
     int Degree;
     bool Mark;
-    FibNode<T>* Parent;
-    unordered_set<FibNode<T>*>* Children;
-    ~FibNode()
+    FibNode<T>* Parent, *Child, *Left, *Right;
+    FibNode(int k, T v)
     {
-        delete Children;
+      Key = k;
+      Val = v;
+      Degree = 0;
+      Mark = false;
+      Parent = Child = Left = Right = NULL;
     }
 };
 
@@ -28,15 +32,18 @@ class FibHeap
     private:
 	    int size;
         FibNode<T>* min;
-        forward_list<FibNode<T>*>* roots;
-        void consolidate(FibNode<T>*);
+        FibNode<T>* roots;
+        void consolidate();
         void link(FibNode<T>*, FibNode<T>*);
         void cut(FibNode<T>*, FibNode<T>*);
         void cascade(FibNode<T>*);
+        void addRoot(FibNode<T>* n);
+        void removeRoot(FibNode<T>* n);
 
 	public:
-		FibHeap();
+		    FibHeap();
         ~FibHeap();
+        FibHeap(int);
 		FibNode<T>* Insert(int, T);
 		FibNode<T>* DeleteMin();
         bool DecreaseKey(FibNode<T>*, int);
@@ -51,26 +58,59 @@ FibHeap<T>::FibHeap()
 {
     size = 0;
     min = NULL;
-    roots = new forward_list<FibNode<T>*>();
+    roots = NULL;
+}
+
+template <class T>
+FibHeap<T>::FibHeap(int i)
+{
+    size = 0;
+    min = NULL;
+    roots = NULL;
 }
 
 template <class T>
 FibHeap<T>::~FibHeap()
 {
-    delete roots;
+    //delete roots;
+}
+
+
+template <class T>
+void FibHeap<T>::addRoot(FibNode<T>* n)
+{
+    n->Left = n->Right = n->Parent = NULL;
+    n->Right = roots;
+    if(n->Right != NULL)
+    {
+      n->Right->Left = n;
+    }
+    roots = n;
+}
+
+template <class T>
+void FibHeap<T>::removeRoot(FibNode<T>* n)
+{
+    if(roots == n)
+    {
+      roots = n->Right;
+    }
+    if(n->Right != NULL)
+    {
+      n->Right->Left = n->Left;
+    }
+    if(n->Left != NULL)
+    {
+      n->Left->Right = n->Right; 
+    }
+    n->Left = n->Right = n->Parent = NULL;
 }
 
 template <class T>
 FibNode<T>* FibHeap<T>::Insert(int k, T v)
 {
-    FibNode<T> * n = new FibNode<T>();
-    n->Key = k;
-    n->Val = v;
-    n->Degree = 0;
-    n->Mark = false;
-    n->Children = new unordered_set<FibNode<T>*>();
-    n->Parent = NULL;
-    roots->push_front(n);
+    FibNode<T> * n = new FibNode<T>(k, v);
+    addRoot(n);
     if(min == NULL || min->Key > k)
     {
         min = n;
@@ -85,21 +125,21 @@ FibNode<T>* FibHeap<T>::DeleteMin()
     FibNode<T> * n = min;
     if(n != NULL)
     {
-        for(auto it = n->Children->begin(); it != n->Children->end(); ++it)
+        removeRoot(n);
+        for(auto it = n->Child; it != NULL; )
         {
-            FibNode<T>* c = *it;
-            roots->push_front(c);
-            c->Parent = NULL;
+            auto nit = it->Right;
+            addRoot(it);
+            it = nit;
         }
         if(size == 1)
         {
             min = NULL;
-            delete roots;
-            roots = new forward_list<FibNode<T>*>();
+            roots = NULL;
         }
         else
         {
-            consolidate(n);
+            consolidate();
         }
         size--;
     }
@@ -108,45 +148,40 @@ FibNode<T>* FibHeap<T>::DeleteMin()
 
 int maxDegree(int n)
 {
-    return 32;
+    //return 32;
     int i = 0;
     while(1<<i++<=n);
     return i*2;
 }
 
 template <class T>
-void FibHeap<T>::consolidate(FibNode<T>* del)
+void FibHeap<T>::consolidate()
 {
     int degree = maxDegree(size);
     FibNode<T>** A = new FibNode<T>*[degree]();
-    forward_list<FibNode<T>*>* nroots = new forward_list<FibNode<T>*>();
-    for(auto it = roots->begin(); it != roots->end(); ++it)
+    for(auto it = roots; it != NULL; )
     {
-        FibNode<T> *  n = *it;
-        if(n==del)
-        {
-          continue;
-        }
-        int d = n->Degree;
+        FibNode<T> *  nit = it->Right;
+        int d = it->Degree;
         while(A[d]!=NULL)
         {
             FibNode<T> * y = A[d];
-            if(n->Key > y->Key)
+            if(it->Key > y->Key)
             {
-                exch(n,y);
+                exch(it,y);
             }
-            link(y,n);
+            link(y,it);
             A[d] = NULL;
             d++;
         }
-        A[d]=n;
+        A[d]=it;
+        it = nit;
     }
     min = NULL;
     for (int i = 0; i < degree; i++)
     {
        if(A[i]!=NULL)
        {
-         nroots->push_front(A[i]);
          if(min == NULL || A[i]->Key < min->Key)
          {
             min = A[i];
@@ -154,14 +189,18 @@ void FibHeap<T>::consolidate(FibNode<T>* del)
        }
     }
     delete[] A;
-    delete roots;
-    roots = nroots;
 }
 
 template <class T>
 void FibHeap<T>::link(FibNode<T>* y,FibNode<T>* n)
 {
-    n->Children->insert(y);
+    removeRoot(y);
+    y->Right = n->Child;
+    if(y->Right!=NULL)
+    {
+      y->Right->Left = y;
+    }
+    n->Child = y;
     n->Degree++;
     y->Mark = false;
     y->Parent = n;
@@ -192,9 +231,18 @@ bool FibHeap<T>::DecreaseKey(FibNode<T>* n, int k)
 template <class T>
 void FibHeap<T>::cut(FibNode<T>* n, FibNode<T>* p)
 {
-    p->Children->erase(n);
-    roots->push_front(n);
-    n->Parent = NULL;
+    if(p->Child == n)
+    {
+      p->Child = n->Right;
+    }
+    if(n->Right != NULL)
+    {
+      n->Right->Left = n->Left; 
+    }if(n->Left != NULL)
+    {
+      n->Left->Right = n->Right; 
+    }
+    addRoot(n);
     n->Mark = false;
 }
 
---
+++ project1/src/tests.h
@@ -0,0 +1,46 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <ctime>
+
+using namespace std;
+
+
+template<typename H, typename N>
+clock_t TestInserts(int n)
+{
+	auto heap = new H(n);
+	auto nodes = new N*[n]();
+	clock_t t = clock();
+	for(int i = 0; i < n; i++)
+	{
+		nodes[i] = heap->Insert(i,i);
+	}
+	t = clock() - t;
+	delete[] nodes;
+	delete heap;
+	return t;
+}
+
+template<typename H, typename N>
+clock_t TestDeleteMin(int n)
+{
+	auto heap = new H(n);
+	auto nodes = new N*[n]();
+
+	for(int i = 0; i < n; i++)
+	{
+		nodes[i] = heap->Insert(i,i);
+	}
+  clock_t t = clock();
+  while(!heap->IsEmpty())
+  {
+    heap->DeleteMin();
+  }
+	t = clock() - t;
+	delete[] nodes;
+	delete heap;
+	return t;
+}
+
+#endif
