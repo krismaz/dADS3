5b73ec88119fefc1d0a38a5584a9f8482cf2bd97
Kris Ebbesen <kve@secoya.dk>
2013-12-29 18:08:33 +0100

    Performance tweaks

--- project3/testQueues.hs
+++ project3/testQueues.hs
@@ -6,28 +6,28 @@ import Queues
 -- General note: use the popped elements or they will be optimized away
 
 -- n times popping an element and pushing another
-test_pop_push :: (Queue, Integer) -> [Integer]
+test_pop_push :: (Queue, Int) -> [Int]
 test_pop_push (q, 0) = []
 test_pop_push (q, n) =
 	let (x, q') = generic_q_pop q in
 	x : test_pop_push(generic_q_push n q', n-1)
 
 -- pop n elements off the queue
-test_pop :: (Queue, Integer) -> [Integer]
+test_pop :: (Queue, Int) -> [Int]
 test_pop (q, 0) = []
 test_pop (q, n) =
 	let (x, q') = generic_q_pop q in
 	x : test_pop(q', n-1)
 
 -- push n elements onto the queue
-test_push :: (Queue, Integer) -> [Integer]
+test_push :: (Queue, Int) -> [Int]
 test_push (q, 0) = let (x, _) = generic_q_pop q in [x]
 test_push (q, n) = test_push(generic_q_push n q, n-1)
 
 
 -- repeatedly push some element to the same queue, 
 -- popping an element as well to make it not be optimized away
-test_repeated_push_pop :: (Queue, Integer) -> [Integer]
+test_repeated_push_pop :: (Queue, Int) -> [Int]
 test_repeated_push_pop (q, n) = 
 	[ let (x,_) = generic_q_pop (generic_q_push i q) in x | i <- [1..n] ]
 
@@ -35,14 +35,14 @@ test_repeated_push_pop (q, n) =
 
 
 -- TEST DATA
-make_test_queue :: (Queue, Integer) -> Queue
+make_test_queue :: (Queue, Int) -> Queue
 make_test_queue (q, 0) = q
 make_test_queue (q, n) = 
 	make_test_queue (generic_q_push n q, n-1)
 
 
 -- less repeated code
-make_nf_test_group :: (String, (Queue, Integer) -> [Integer], Integer) -> Benchmark
+make_nf_test_group :: (String, (Queue, Int) -> [Int], Int) -> Benchmark
 make_nf_test_group (s, f, n) =
 	let listQ = make_test_queue (generic_q_make TList, n) in
 	let strictQ = make_test_queue (generic_q_make TStrict, n) in
@@ -55,7 +55,7 @@ make_nf_test_group (s, f, n) =
 		, bench "realtime" $ nf f (realtimeQ, 1000)
 		]
 
-make_sized_array_test_group :: Integer -> Benchmark
+make_sized_array_test_group :: Int -> Benchmark
 make_sized_array_test_group n = 
 	bgroup (show n)
 		[ make_nf_test_group ("test_pop_push", test_pop_push, n)
